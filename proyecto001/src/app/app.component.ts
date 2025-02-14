36d13baf00cce83cc781ed241906106570062482
Ordenador Clase <mvc478@gmail.com>
2025-02-14 21:15:15 +0100

    Update 14/02 21:15

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto001';
+  nombre='pedro';
+  edad=21;
+  fumador=true;
+  sueldos=[1200,1500,1700];
+
+  provincias = [
+    {id:1, nombre:'Madrid'},
+    {id:2, nombre:'Barcelona'},
+    {id:3, nombre:'Sevilla'},
+    {id:4, nombre:'Valencia'}
+  ];
+
+  esFumador(){
+    return this.fumador?"Es fumador":"No es fumador";
+  }
+
+  diHola(){
+    return alert("Hola");
+  }
+  contador = 0;
+
+  incrementar(){
+    this.contador++;
+  }
+  decrementar(){
+    this.contador--;
+  }
 }
