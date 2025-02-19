aac27e170366c3ff9d3c33a0edaa93867fb35f3f
Ordenador Clase <mvc478@gmail.com>
2025-02-19 21:15:16 +0100

    Proyecto003 Funcional

---
+++ proyecto003/src/app/dado/dado.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  standalone: false,
+  templateUrl: './dado.component.html',
+  styleUrl: './dado.component.css'
+})
+export class DadoComponent {
+  valor!:number;
+
+  constructor(){
+    this.valor = this.generaAleatorio();
+  }
+
+  generaAleatorio(){
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+}
